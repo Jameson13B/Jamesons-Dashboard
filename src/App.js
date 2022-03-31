42fcad7512d4f45b75c80941c6a77efb0cb33f1f
Jameson.Brown <jameson.brown@mx.com>
2022-03-31 13:50:05 -0600

    Initial commit: Created README, cleaned up repo, and added firebase.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { db, COLLECTIONS } from './utils/firebase'
+import { doc, onSnapshot } from 'firebase/firestore'
 
 function App() {
+  const styles = getStyles()
+  const docID = '1tO1RY8iXzIuMdzz9knG'
+  const [recipe, setRecipe] = useState(null)
+
+  useEffect(() => {
+    const docRef = doc(db, COLLECTIONS.RECIPES_BAKING, docID)
+
+    const unsub = onSnapshot(docRef, (doc) => setRecipe(doc.data()))
+
+    return () => unsub()
+  }, [docID])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div style={styles.appContainer}>
+      <header style={styles.appHeader}>
+        <h1>Jameson Dashboard</h1>
+        <h2 style={styles.subHead}>Ideas for the dashboard:</h2>
+        <ul style={styles.unorderdList}>
+          <li>Food/Recipes</li>
+          <ul style={styles.unorderdList}>
+            <li>Baked goods</li>
+            <li>First baked recipe: {recipe ? recipe.name : 'Loading...'}</li>
+            <ul>
+              <li>{recipe ? recipe.description : 'Loading...'}</li>
+            </ul>
+            <li>Favorite recipes</li>
+          </ul>
+          <li>Beer/Cider Recipes</li>
+          <ul style={styles.unorderdList}>
+            <li>Share recipes</li>
+            <li>Provide updates</li>
+          </ul>
+          <li>Travel/Adventure</li>
+          <ul style={styles.unorderdList}>
+            <li>Trip reports</li>
+            <li>Trip planning/invites</li>
+          </ul>
+          <li>Favorites SLC Eats</li>
+          <ul style={styles.unorderdList}>
+            <li>Favorites by category</li>
+            <li>Restaurant sampler parties</li>
+          </ul>
+        </ul>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+const getStyles = () => ({
+  appContainer: {
+    textAlign: 'center',
+  },
+  appHeader: {
+    backgroundColor: '#282c34',
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 'calc(10px + 2vmin)',
+    color: 'white',
+  },
+  subHead: {
+    marginBottom: 0,
+  },
+  unorderdList: {
+    textAlign: 'left',
+    marginBottom: 20,
+  },
+})
---
+++ src/utils/firebase.js
@@ -0,0 +1,37 @@
+import { initializeApp } from 'firebase/app'
+import 'firebase/firestore'
+import { getFirestore, collection } from 'firebase/firestore'
+// import { getStorage } from 'firebase/storage'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_ID,
+  appId: process.env.REACT_APP_APP_ID,
+}
+
+// Initialize Firebase
+const Firebase = initializeApp(firebaseConfig)
+
+// DATABASE INSTANCES:
+export const db = getFirestore(Firebase)
+export const COLLECTIONS = {
+  RECIPES_BAKING: 'dashboard-recipes-baking',
+  // Coming soon:
+  // RECIPES_FAVORITE: 'dashboard-recipes-favorite',
+  // BREW_RECIPES: 'dashboard-brew-recipes',
+  // BREW_UPDATES: 'dashboard-brew-updates',
+  // TRAVEL_REPORTS: 'dashboard-travel-reports',
+  // TRAVEL_GROUP: 'dashboard-travel-group',
+  // EATS_FAVORITES: 'dashboard-eats-favorites',
+  // EATS_GROUP: 'dashboard-eats-group',
+}
+export const bakingDB = collection(db, COLLECTIONS.RECIPES_BAKING)
+// const serverTimestamp = () => firebase.firestore.FieldValue.serverTimestamp()
+// const addArray = item => firebase.firestore.FieldValue.arrayUnion(item);
+// const removeArray = item => firebase.firestore.FieldValue.arrayRemove(item);
+
+// STORAGE INSTANCES:
+// const storage = getStorage(Firebase)
