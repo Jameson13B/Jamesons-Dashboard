8d713e39e0c244f3a139742459f0eafc0d69a16a
Jameson.Brown <jameson.brown@mx.com>
2022-04-09 17:49:08 -0600

    Refactored to include new RecipeButton and multiple recipes

--- src/views/Recipes.js
+++ src/views/Recipes.js
@@ -1,30 +1,95 @@
 import React, { useEffect, useState } from 'react'
-import { doc, onSnapshot } from 'firebase/firestore'
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  startAfter,
+  endBefore,
+  query,
+} from 'firebase/firestore'
+import { css } from 'glamor'
 
 import { db, COLLECTIONS } from '../utils/firebase'
+import { RecipeButton } from '../components/RecipeButton'
 
 export const Recipes = () => {
-  const [recipe, setRecipe] = useState(null)
+  const [recipes, setRecipes] = useState([])
   const styles = getStyles()
-  const docID = '1tO1RY8iXzIuMdzz9knG'
+  const [startDoc, setStartDoc] = useState(null)
+  const [last, setLast] = useState(null)
 
   useEffect(() => {
-    const docRef = doc(db, COLLECTIONS.RECIPES_BAKING, docID)
+    const collectionRef = query(
+      collection(db, COLLECTIONS.RECIPES_BAKING),
+      orderBy('name'),
+      limit(3),
+    )
 
-    const unsub = onSnapshot(docRef, (doc) => setRecipe(doc.data()))
+    getDocs(collectionRef).then((snapshots) => {
+      const newRecipes = []
 
-    return () => unsub()
-  }, [docID])
+      snapshots.forEach((doc) => newRecipes.push({ id: doc.id, ...doc.data() }))
+
+      setStartDoc(snapshots.docs[0].id)
+      setLast(snapshots.docs[snapshots.docs.length - 1])
+      setRecipes(newRecipes)
+    })
+  }, [])
+
+  const getPrev = () => {
+    const next = query(
+      collection(db, COLLECTIONS.RECIPES_BAKING),
+      orderBy('name'),
+      limit(3),
+      endBefore(last),
+    )
+
+    getDocs(next).then((snapshots) => {
+      const newRecipes = []
+
+      snapshots.forEach((doc) => newRecipes.push({ id: doc.id, ...doc.data() }))
+
+      setLast(snapshots.docs[snapshots.docs.length - 1])
+      setRecipes(newRecipes)
+    })
+  }
+  const getMore = () => {
+    const next = query(
+      collection(db, COLLECTIONS.RECIPES_BAKING),
+      orderBy('name'),
+      limit(3),
+      startAfter(last),
+    )
+
+    getDocs(next).then((snapshots) => {
+      const newRecipes = []
+
+      snapshots.forEach((doc) => newRecipes.push({ id: doc.id, ...doc.data() }))
+
+      setLast(snapshots.docs[snapshots.docs.length - 1])
+      setRecipes(newRecipes)
+    })
+  }
+
+  if (recipes.length === 0) {
+    return <h1 style={styles.title}>Loading...</h1>
+  }
 
   return (
-    <div style={styles.container}>
-      {recipe ? (
-        <>
-          <h1 style={styles.title}>{recipe.name}</h1>
-          <p style={styles.description}>{recipe.description}</p>
-        </>
-      ) : (
-        <h1 style={styles.title}>Loading...</h1>
+    <div className={css(styles.container)}>
+      {recipes[0].id !== startDoc && (
+        <button className={css(styles.navButton)} onClick={getPrev}>
+          <p>PREV</p>
+        </button>
+      )}
+      {recipes.map((recipe) => (
+        <RecipeButton key={recipe.id} recipe={recipe} />
+      ))}
+      {recipes.length === 3 && (
+        <button className={css(styles.navButton)} onClick={getMore}>
+          <p>MORE</p>
+        </button>
       )}
     </div>
   )
@@ -32,9 +97,45 @@ export const Recipes = () => {
 
 const getStyles = () => ({
   container: {
-    background: 'black',
+    display: 'flex',
+    '@media(max-width: 768px)': {
+      flexDirection: 'column',
+      height: '100%',
+    },
+  },
+  navButton: {
+    background: 'transparent',
+    borderStyle: 'none',
+    cursor: 'pointer',
     height: '100vh',
-    width: '100vw',
+    padding: '0',
+    flex: 1,
+    maxWidth: '50px',
+    '&:hover': {
+      background: '#232323',
+    },
+    '& p': {
+      // The next three styles create gradient text
+      background: 'linear-gradient(to right, #E1FF31, #8DFFA8)',
+      WebkitBackgroundClip: 'text',
+      WebkitTextFillColor: 'transparent',
+      fontSize: '1.5em',
+      fontWeight: '900',
+      margin: '0',
+      padding: '0 10px',
+      textOrientation: 'upright',
+      writingMode: 'vertical-rl',
+      '@media(max-width: 768px)': {
+        writingMode: 'initial',
+      },
+    },
+    '@media(max-width: 768px)': {
+      height: 'initial',
+      padding: '10px 0',
+      maxHeight: '50px',
+      maxWidth: 'initial',
+      width: '100%',
+    },
   },
   title: {
     // The next three styles create gradient text
