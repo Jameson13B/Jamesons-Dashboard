9cf9899c441859ebe691555e075eb97b897d9c65
Jameson.Brown <jameson.brown@mx.com>
2022-04-18 12:31:54 -0600

    Refactored UtililityButton to a drawer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Route, Switch } from 'wouter'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faFireBurner } from '@fortawesome/free-solid-svg-icons'
 
 import { Home } from './views/Home'
 import { Recipes } from './views/Recipes'
 import { RecipeDetails } from './views/RecipeDetails'
-import { UtilityButton } from './components/UtilityButton'
+import { UtilityButtons } from './components/UtilityButtons'
 
 function App() {
   const styles = getStyles()
@@ -20,7 +22,21 @@ function App() {
         </Route>
         <Route path="/recipes/:id">{(params) => <RecipeDetails recipeId={params.id} />}</Route>
       </Switch>
-      <UtilityButton content="🏠" path="/" position="left" />
+      <UtilityButtons
+        buttons={[
+          {
+            content: <FontAwesomeIcon icon={faHouse} color="black" size="lg" />,
+            path: '/',
+            color: 'linear-gradient(to right, #E1FF31, #8dffa8)',
+          },
+          {
+            content: <FontAwesomeIcon icon={faFireBurner} color="black" size="lg" />,
+            path: '/recipes',
+            color: 'linear-gradient(to right, #E1FF31, #8dffa8)',
+          },
+        ]}
+        position="left"
+      />
     </div>
   )
 }
---
+++ src/components/UtilityButtons.js
@@ -0,0 +1,110 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDoubleDown, faAngleDoubleUp } from '@fortawesome/free-solid-svg-icons'
+import { useLocation } from 'wouter'
+
+const positions = ['left', 'right']
+
+export const UtilityButtons = (props) => {
+  const { buttons, position = 'left' } = props
+  const [open, toggleOpen] = useState(false)
+  const [_, setLocation] = useLocation() // eslint-disable-line no-unused-vars
+  const drawerRef = useRef()
+  const styles = getStyles()
+
+  useEffect(() => {
+    // Temporary not of position prop:
+    if (position) {
+      console.warn('Position support coming soon. Defaulting to "left".')
+    }
+    // if (!positions.includes(position)) {
+    //   console.error('Position Error: Please choose from ["left", "right"]')
+    // }
+  }, [position])
+
+  if (!positions.includes(position)) {
+    return <p style={styles.error}>Position Error</p>
+  }
+
+  return (
+    <div style={styles.drawer} ref={drawerRef}>
+      <div
+        style={styles.drawHandle}
+        className="handle"
+        onClick={() => {
+          toggleOpen(!open)
+          drawerRef.current.style.transform = `translate(0px, ${open ? '0' : '-75px'})`
+        }}
+      >
+        {open ? (
+          <FontAwesomeIcon icon={faAngleDoubleDown} style={styles.buttonHelper} />
+        ) : (
+          <FontAwesomeIcon icon={faAngleDoubleUp} style={styles.buttonHelper} />
+        )}
+      </div>
+      {buttons.map((button, i) => (
+        <div
+          key={i}
+          onClick={() => setLocation(button.path)}
+          style={{
+            ...styles.homeButton,
+            background: button.color ?? 'slategrey',
+          }}
+        >
+          {button.content}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+const getStyles = () => ({
+  error: {
+    color: 'red',
+    fontWeight: 'bolder',
+    position: 'fixed',
+    top: '10px',
+    left: '45%',
+  },
+  drawer: {
+    background: 'slategrey',
+    height: '75px',
+    position: 'fixed',
+    bottom: '-75',
+    width: '100%',
+    transition: 'transform .5s',
+    // Flexbox
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  drawHandle: {
+    background: 'slategrey',
+    borderRadius: '5px 5px 0 0',
+    color: 'white',
+    padding: '13px 35px',
+    position: 'absolute',
+    top: '-26px',
+    left: '15px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  homeButton: {
+    borderRadius: '15px',
+    color: 'white',
+    cursor: 'pointer',
+    height: '60px',
+    width: '70px',
+    // Flexbox
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    userSelect: 'none',
+  },
+  buttonHelper: {
+    position: 'absolute',
+    fontWeight: 'bold',
+  },
+})
